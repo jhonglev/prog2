648d6eedc36d4385420170b02043e71c24d699cc
jhonglev <60974078+jhonglev@users.noreply.github.com>
2020-11-13 20:21:09 -0300

    Add files via upload

---
+++ python_flask_app/README.md
@@ -0,0 +1,7 @@
+# Frontend: prints
+
+## Diagrama UML
+![erro](av5_1.PNG)
+
+## Teste
+![erro](av5_2.PNG)
---
+++ python_flask_app/back-end/models.py
@@ -0,0 +1,100 @@
+from config import *
+
+class Sapato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    modelo = db.Column(db.String(128))
+    tamanho = db.Column(db.String(128))
+    cor = db.Column(db.String(128))
+
+    sapateira = db.relationship("Sapateira", back_populates="sapato")
+
+
+    def __str__(self):
+        return f"{self.id} - {self.modelo}, {self.tamanho}, {self.cor}"
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "modelo": self.modelo,
+            "tamanho": self.tamanho,
+            "cor": self.cor
+        }
+
+
+class Pessoa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(128))
+    idade = db.Column(db.String(128))
+
+    sapateira = db.relationship("Sapateira", back_populates="pessoa")
+
+
+    def __str__(self):
+        return f"{self.id} - {self.nome}, {self.idade}"
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "idade": self.idade
+        }
+
+
+class Sapateira(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    capacidade = db.Column(db.Integer)
+    cor = db.Column(db.String(128))
+    material = db.Column(db.String(128))
+
+    sapato_id = db.Column(db.Integer, db.ForeignKey(Sapato.id), nullable=False)
+    sapato = db.relationship("Sapato", back_populates="sapateira")
+
+    pessoa_id = db.Column(db.Integer, db.ForeignKey(Pessoa.id), nullable=False)
+    pessoa = db.relationship("Pessoa", back_populates="sapateira")
+
+
+    def __str__(self):
+        return f"Sapateira: {self.id} - {self.capacidade}, {self.cor}, {self.material}" +\
+               f"\nPessoa: {self.pessoa}\nSapato: {self.sapato}"
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "capacidade": self.capacidade,
+            "cor": self.cor,
+            "material": self.material,
+            "sapato_id": self.sapato_id,
+            "sapato": self.sapato,
+            "pessoa_id": self.pessoa_id,
+            "pessoa": self.pessoa,
+        }
+
+
+if __name__ == "__main__":
+    if os.path.exists(db_path):
+        os.remove(db_path)
+    db.create_all()
+
+    s1 = Sapato(modelo="Jordan", tamanho="40", cor="Preto")
+    db.session.add(s1)
+
+    p1 = Pessoa(nome="Michael", idade=57)
+    db.session.add(p1)
+
+    sapateira1 = Sapateira(capacidade=7, cor="Preto", material="Metal",
+                sapato=s1, pessoa=p1)
+    db.session.add(p1)
+
+    db.session.commit()
+
+    print(s1)
+    print(s1.json())
+    print('\n')
+    print(p1)
+    print(p1.json())
+    print('\n')
+    print(sapateira1)
+    print(sapateira1.json())
